d879c939b2cb639904529f419b9e9a8b7bc10e0b
thanhphucnguyen1810 <thanhphuc1810.work@gmail.com>
2025-11-06 10:33:15 +0700

    feat(api): add advanced product query, best-seller, new-arrivals, det…

---
+++ backend/src/seeder.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import Product from '~/models/product.model'
+import User from '~/models/user.model'
+import Cart from '~/models/cart.model'
+import products from '~/data/products.data'
+import { CONNECT_DB } from './config/mongodb'
+
+
+// Connect to MongoDB
+CONNECT_DB()
+
+// Function to seed data
+const seedData = async () => {
+  try {
+    // Clear existing data
+    await Product.deleteMany()
+    await User.deleteMany()
+    await Cart.deleteMany()
+
+    // Create a default admin User
+    const createUser = await User.create({
+      name: 'Admin User',
+      email: 'admin@example.com',
+      password: '12345678',
+      role: 'admin'
+    })
+
+    // Assign the default user ID to each product
+    const userID = createUser._id
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID }
+    })
+
+    // Insert the products into the database
+    await Product.insertMany(sampleProducts)
+    console.log('Product data seeded successfully!')
+
+    process.exit()
+  } catch (error) {
+    console.error('Error seeding the data: ', error)
+    process.exit(1)
+
+  }
+}
+
+seedData()
+
