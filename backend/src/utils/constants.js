ddfcd31983533abc9df3e1732859fd456c6d979d
thanhphucnguyen1810 <thanhphuc1810.work@gmail.com>
2026-04-16 11:43:47 +0700

    fix: correct whitelist domain syntax for production deploy

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -2,7 +2,7 @@
 // NHỮNG DOMAIN ĐƯỢC PHÉP TRUY CẬP TỚI TÀI NGUYÊN CỦA SERVER
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
-  https://fashion-shop-nl.vercel.app
+  'https://fashion-shop-nl.vercel.app'
   
   // process.env.FRONTEND_URL,
   // Ngrok frontend
